f089755b3789d20f9cadcb71cd3f27704b9dd392
Aaron McCarthy <amccarthy9904@gmail.com>
2018-05-03 13:56:35 -0600

    displays list of product names on main window

--- OrderSubmiter/OrderSubmiter/MainWindow.xaml.cs
+++ OrderSubmiter/OrderSubmiter/MainWindow.xaml.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,16 +22,33 @@ namespace OrderSubmiter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread orderThread;
         private Fetcher fetch;
+        private ProductFactory factory = new ProductFactory();
+         
         public MainWindow()
         {
             InitializeComponent();
-            fetch = new Fetcher();
+            this.fetch = new Fetcher();
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            fetch.getProducts();
+            dynamic prods = fetch.getProducts();
+            var prodItems = factory.getItems(prods);
+            foreach (UIElement item in prodItems)
+            {
+                this.productHolder.Children.Add(item);
+            }
+        }
+
+        protected void textBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void scrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
         }
     }
 }
