cf3813f227e2b738b028c80e7a45785c4f2f20dc
Aaron McCarthy <amccarthy9904@gmail.com>
2018-05-03 13:55:51 -0600

    deserializing json into dynamic object properly

--- OrderSubmiter/OrderSubmiter/Fetcher.cs
+++ OrderSubmiter/OrderSubmiter/Fetcher.cs
@@ -5,6 +5,9 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace OrderSubmiter
 {
@@ -15,6 +18,8 @@ namespace OrderSubmiter
         private string apiKey = "136085";
         private string credentials;
         private WebClient client;
+        private static readonly JsonSerializerSettings settings = new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore};
+        //private string pattern = "({\"id\".* \"del)";
 
         public Fetcher()
         {
@@ -22,19 +27,17 @@ namespace OrderSubmiter
             //setup connectiuon with API
             this.client = new WebClient();
             this.credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes("miniproject:Pr!nt123"));
-            //this.client.Credentials = new NetworkCredential("miniproject", "Pr!nt123");
             this.client.UseDefaultCredentials = true;
         }
 
-        public void getProducts()
+        public dynamic getProducts()
         {
-            this.client.Headers[HttpRequestHeader.Authorization] = this.credentials;
+            this.client.Headers[HttpRequestHeader.Authorization] = $"Basic {this.credentials}";
             this.client.Headers.Add("Content-Type", "application/json");
-            //this.client.QueryString.Add("apikey", this.apiKey);
-            //this.client.QueryString.Add("Content - Type", "application/json");
-
-            string str = this.client.DownloadString(api);
-            Console.WriteLine(str);
+            ArrayList prodList = new ArrayList();
+            string prodStr = this.client.DownloadString(api);
+            dynamic temp = JsonConvert.DeserializeObject(prodStr, settings);
+            return temp.results.data;
             //return a list of all products
         }
 
