56241e02d0b03ff0ffd8abef2b8b5c47c0c9072a
Aaron McCarthy <amccarthy9904@gmail.com>
2018-05-04 18:48:07 -0600

    implemented sending orders. need to use just HTTP client for both sending orders and recieveing products

--- OrderSubmiter/OrderSubmiter/Fetcher.cs
+++ OrderSubmiter/OrderSubmiter/Fetcher.cs
@@ -8,44 +8,96 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Collections;
 using System.Text.RegularExpressions;
+using System.Net.Http.Headers;
 
 namespace OrderSubmiter
 {
     class Fetcher
-    {
-        private string api = "https://testapi.pfl.com/products?apikey=136085";
-        private string products = "/products";
-        private string apiKey = "136085";
-        private string credentials;
-        private WebClient client;
-        private static readonly JsonSerializerSettings settings = new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore};
-        //private string pattern = "({\"id\".* \"del)";
+    {   
+        /// <summary>
+        /// uri to interact with API
+        /// </summary>
+        private string api = "https://testapi.pfl.com/{0}?apikey={1}";
 
+        /// <summary>
+        /// api{0} substring to get products
+        /// </summary>
+        private string products = "/products";
+
+        /// <summary>
+        /// api{0} substring to make orders 
+        /// </summary>
+        private string orders = "/orders";
+
+        /// <summary>
+        /// api{1} substring to access api
+        /// </summary>
+        private string apiKey = "136085";
+
+        /// <summary>
+        /// user and pass for api authentification
+        /// </summary>
+        private string credentials;
+
+        /// <summary>
+        /// interacts with server - gets product list
+        /// </summary>
+        private WebClient webClient;
+
+        /// <summary>
+        /// interacts with server - sends orders
+        /// </summary>
+        private HttpClient httpClient;
+
+        /// <summary>
+        /// JsonSettings needed in more than one place
+        /// </summary>
+        private static readonly JsonSerializerSettings settings = new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore};
+
+        /// <summary>
+        /// Inits Fetcher. sends and recieves data fom API server
+        /// </summary>
         public Fetcher()
         {
-            //TODO
-            //setup connectiuon with API
-            this.client = new WebClient();
+            this.webClient = new WebClient();
+            this.httpClient = new HttpClient();
             this.credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes("miniproject:Pr!nt123"));
-            this.client.UseDefaultCredentials = true;
+            this.webClient.UseDefaultCredentials = true;
         }
 
+        /// <summary>
+        /// gets a list of all products availible
+        /// </summary>
+        /// <returns>Dynamic obj with structure of json returned by API</returns>
         public dynamic getProducts()
         {
-            this.client.Headers[HttpRequestHeader.Authorization] = $"Basic {this.credentials}";
-            this.client.Headers.Add("Content-Type", "application/json");
-            ArrayList prodList = new ArrayList();
-            string prodStr = this.client.DownloadString(api);
+            this.webClient.Headers[HttpRequestHeader.Authorization] = $"Basic {this.credentials}";
+            this.webClient.Headers.Add("Content-Type", "application/json");
+            string prodStr = this.webClient.DownloadString(string.Format(api, products, apiKey));
             dynamic temp = JsonConvert.DeserializeObject(prodStr, settings);
             return temp.results.data;
-            //return a list of all products
         }
 
-        public void orderProduct(object product)
+        /// <summary>
+        /// orders the specified product with the specified amount
+        /// </summary>
+        /// <param name="product">product to order</param>
+        /// <returns>string containing order information</returns>
+        public async void orderProduct(Order product, OrderButton button)
         {
-            //orers the product from the API
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", this.credentials);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var temp = JsonConvert.SerializeObject(product, Formatting.Indented);
+            HttpResponseMessage response = await httpClient.PostAsJsonAsync(string.Format(api, orders, apiKey), product);
+           
+            if (response.IsSuccessStatusCode)
+            {
+                button.Content = "Success";
+            }
+            else
+            {
+                button.Content = "Failure";
+            }
         }
-
-
     }
 }
