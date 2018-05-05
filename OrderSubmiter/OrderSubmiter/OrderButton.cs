9d5e9897178a3f90e84dea23055690f3f3bb4a65
Aaron McCarthy <amccarthy9904@gmail.com>
2018-05-04 18:53:03 -0600

    extends Button, used to trigger creation of order menu

---
+++ OrderSubmiter/OrderSubmiter/OrderButton.cs
@@ -0,0 +1,26 @@
+﻿namespace OrderSubmiter
+{   
+    /// <summary>
+    /// button used to open the order menu and to send an order
+    /// </summary>
+    class OrderButton : System.Windows.Controls.Button
+    {
+        /// <summary>
+        /// json description of product to order
+        /// </summary>
+        public dynamic product { get; set; }
+
+        /// <summary>
+        /// associated quantity box
+        /// </summary>
+        public QuantBox quantBox { get; set; }
+
+        /// <summary>
+        /// creates and returns an OrderButton and sets height to 35
+        /// </summary>
+        public OrderButton()
+        {
+            this.Height = 35;
+        }
+    }
+}
