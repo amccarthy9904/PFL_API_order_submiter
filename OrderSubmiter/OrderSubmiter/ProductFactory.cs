719ffa7be318c2dc279cc9ad2f80ac44bb6f8b9d
Aaron McCarthy <amccarthy9904@gmail.com>
2018-05-04 18:51:15 -0600

    cleaned up, commented code

--- OrderSubmiter/OrderSubmiter/ProductFactory.cs
+++ OrderSubmiter/OrderSubmiter/ProductFactory.cs
@@ -1,51 +1,75 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Collections;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OrderSubmiter
 {
+    /// <summary>
+    /// Populates the Main StackPanel with UIElements based off of 
+    /// the json object recieved from Fetcher
+    /// </summary>
     class ProductFactory
     {
-        public ProductFactory()
-        {
+        /// <summary>
+        /// preset string template to display product Ids
+        /// </summary>
+        private string idLine = "Product ID: {0}";
 
-        }
+        /// <summary>
+        /// preset string template to display product descriptions
+        /// </summary>
+        private string descLine = "Description: {0}";
 
+        /// <summary>
+        /// creates an ArrayList of UIElements to be displayed in the main StackPanel
+        /// for every product it makes a border, StackPanel, textBlock, and Button
+        /// TODO:
+        ///     insert image on far left of row
+        /// </summary>
+        /// <param name="products">Json object describing all products</param>
+        /// <returns>ArrayList of UIElements to be displayed in the main StackPanel</returns>
         public ArrayList getItems(dynamic products)
         {
             ArrayList items = new ArrayList();
-            foreach (var product in products)
+            foreach (var product in (dynamic)products)
             {
-                ListBoxItem listItem = new ListBoxItem();
-                listItem.Content = product.name;
-                listItem.Height = 30;
-                listItem.HorizontalAlignment = HorizontalAlignment.Left;
-                //listItem.VerticalAlignment = VerticalAlignment.Top;
-                //listBox.Margin = new Thickness(50, 150, 0, 0);
+                ///create the boarder for each row
+                Border border = new Border();
+                border.BorderThickness = new Thickness(2,0,2,2);
+                border.BorderBrush = Brushes.LightGray;
 
-                items.Add(listItem);
+                ///create the stackPanel that holds the row
+                StackPanel panel = new StackPanel();
+                panel.Orientation = Orientation.Horizontal;
+
+                ///add text that desccripes the given product
+                TextBlock text = new TextBlock();
+                text.Width = 250;
+                text.TextWrapping = TextWrapping.Wrap;
+                text.Inlines.Add(product.name.ToString());
+                text.Inlines.Add(new LineBreak());
+                text.Inlines.Add(string.Format(idLine, product.productID));
+                text.Inlines.Add(new LineBreak());
+                if (product.description.ToString().Length > 0)
+                {
+                    text.Inlines.Add(string.Format(descLine, product.description));
+                    text.Inlines.Add(new LineBreak());
+                }
+
+                ///add button that starts an order
+                OrderButton orderButton = new OrderButton();
+                orderButton.product = product;
+                orderButton.Click += new RoutedEventHandler(MainWindow.makeOrder);
+                orderButton.Height = 35;
+                orderButton.Content = "Start Order";
+                
+                panel.Children.Add(text);
+                panel.Children.Add(orderButton);
+                border.Child = panel;
+                items.Add(border);
             }
-
-
-            ArrayList boi = new ArrayList();
-
-            ListBoxItem listBoi = new ListBoxItem();
-            listBoi.Content = "BOI";
-            boi.Add(listBoi);
-
             return items;
         }
     }
