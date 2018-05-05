60da66cd3cb72b9ac2e97360ed8624c8000f1570
Aaron McCarthy <amccarthy9904@gmail.com>
2018-05-04 18:55:11 -0600

    removed unesscesary using statements

--- OrderSubmiter/OrderSubmiter/Fetcher.cs
+++ OrderSubmiter/OrderSubmiter/Fetcher.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
-using System.Collections;
-using System.Text.RegularExpressions;
 using System.Net.Http.Headers;
 
 namespace OrderSubmiter
